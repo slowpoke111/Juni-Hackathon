de63809d2d7f7024a0852a383e3a2a618296081a
slowpoke111 <110692534+slowpoke111@users.noreply.github.com>
2024-06-26 21:19:23 -0400

    Created easier to use function

--- getData.py
+++ getData.py
@@ -1,6 +1,6 @@
 import overpy
 
-def fetchAmenitiesOfType(latitude, longitude, amenity_type, radius):
+def _fetchAmenitiesOfType(latitude, longitude, amenity_type, radius):
     api = overpy.Overpass()
     
     
@@ -12,6 +12,13 @@ def fetchAmenitiesOfType(latitude, longitude, amenity_type, radius):
     
     result = api.query(query)
     return result
+
+def fetchAmenitiesOfType(latitude, longitude, amenity_type, radius):
+    amenities = fetchAmenitiesOfType(latitude, longitude, amenity_type, radius)
+
+    for node in amenities.nodes:
+        name = node.tags.get('name', 'Unnamed')  # Defualt to unnamed
+        return {"amenity":amenity_type, "name": name, "lat":node.lat, "lon":node.lon}
 
 latitude = 39.9526 
 longitude = -75.1652  
