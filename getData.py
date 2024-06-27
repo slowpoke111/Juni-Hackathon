c900f4553388c5c097fbf6354455ac01fb066363
slowpoke111 <110692534+slowpoke111@users.noreply.github.com>
2024-06-27 17:02:05 -0400

    Added getRainChance and fixed locations

--- getData.py
+++ getData.py
@@ -1,4 +1,7 @@
 import overpy
+import requests
+from requests.exceptions import HTTPError
+import json
 
 def _fetchAmenitiesOfType(latitude:float, longitude:float, amenity_type:str, radius:int):
     api = overpy.Overpass()
@@ -22,6 +25,37 @@ def fetchAmenitiesOfType(latitude:float, longitude:float, amenity_type:str, radi
     
     return amenitiesList
 
+#https://api.weather.gov/gridpoints/PHI/46,85/forecast
+def _getRainChance(url,period=0):
+    try:
+        response = requests.get(url)
+        response.raise_for_status() 
+
+    except HTTPError as http_err:
+        raise HTTPError(f"{http_err} in forecast url")
+    except Exception as e:
+        raise Exception(f"Error: {e} in forecast url")
+    else:
+        x = response.json()["properties"]["periods"][period]["probabilityOfPrecipitation"]["value"]
+        return 0 if x==None else int(x)
+#https://api.weather.gov/gridpoints/PHI/46,85/forecast
+
+def getRainChance(lat:float,long:float,period:int=0):
+    baseURL = f"https://api.weather.gov/points/{lat},{long}"
+
+    try:
+        response = requests.get(baseURL)
+        response.raise_for_status() #Handle status codes between 400-600 
+
+    except HTTPError as http_err:
+        raise HTTPError(f"{http_err} in base url")
+    except Exception as e:
+        raise Exception(f"Error: {e} in base url")
+    else:
+        return _getRainChance(response.json()["properties"]["forecast"], period)
+
+
+"""
 latitude = 39.9526 
 longitude = -75.1652  
 amenity_type = "restaurant"  # https://wiki.openstreetmap.org/wiki/Key:amenity
@@ -33,3 +67,4 @@ if __name__ == "__main__":
     for node in amenities.nodes:
         name = node.tags.get('name', 'Unnamed')  # Defualt to unnamed
         print(f"{amenity_type}: {name} ({node.lat}, {node.lon})")
+"""
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from getData import fetchAmenitiesOfType
+from getData import *
 
-#Philadelphia 
-latitude = 39.9526 
-longitude = -75.1652  
+latitude = 40.1546
+longitude = -75.2216
 
 amenities = fetchAmenitiesOfType(latitude,longitude,"restaurant",1000)
 
 for i in amenities:
     print(i)
+
+print(getRainChance(latitude,longitude))
