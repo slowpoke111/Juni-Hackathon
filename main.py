c4cf781d1c68ba8599e9aa67b719073264a2f0f7
r1verpuppy <coconnor880@gmail.com>
2024-07-01 17:26:23 -0400

    scroll bar

--- main.py
+++ main.py
@@ -7,31 +7,36 @@ def fetch_amenities():
     try:
         zip_code = zip_entry.get()
         radius = float(radius_entry.get())
-        
+
         latitude, longitude = utils.zipToLatLong(int(zip_code))
-        
+
         indoorActivities = ["restaurant", "arts_centre", "cinema", "exhibition_centre", "music_venue", "planetarium", "theatre"]
-        outdoorActivities = ["ice_cream", "stage"]
-        
+        outdoorActivities = ["ice_cream", "stage", "park"]
+
         amenities = fetchAmenitiesOfTypeMultiple(latitude, longitude, indoorActivities, utils.miToMeters(radius))
-        
+
         if getRainChance(latitude, longitude) < 20:
             outdoorAmenities = fetchAmenitiesOfTypeMultiple(latitude, longitude, outdoorActivities, utils.miToMeters(radius))
             amenities.extend(outdoorAmenities)
-        
+
         result_text = ""
         for amenity in amenities:
             result_text += f"{amenity}\n" #Format later
-        
+
         #Testing
         if amenities:
             lastAmenity = amenities[-1]
             address = utils.latLongtoAddress(lastAmenity["lat"], lastAmenity["long"])
             result_text += f"\nAddress of the last amenity:\n{address}"
-        
-        result_label.configure(text=result_text)
+
+        result_label.configure(state="normal")
+        result_label.delete("0.0", "end")
+        result_label.insert("0.0", result_text)
+        print(result_text)
+        result_label.configure(state="disabled")
     except Exception as e:
-        result_label.configure(text=f"Error: {e}")
+        #result_label.configure(text=f"Error: {e}")
+        print(e)
 
 #TODO: Add loading bar
 app = ctk.CTk()
@@ -39,6 +44,7 @@ app.geometry("600x400")
 app.title("Untitled")
 
 ctk.set_appearance_mode("dark")
+
 
 zip_label = ctk.CTkLabel(app, text="ZIP Code:")
 zip_label.grid(row=0, column=0, padx=10, pady=10)
@@ -55,7 +61,11 @@ radius_entry.grid(row=1, column=1, padx=10, pady=10)
 fetch_button = ctk.CTkButton(app, text="Fetch Amenities", command=fetch_amenities)
 fetch_button.grid(row=2, column=0, columnspan=2, pady=20)
 
-result_label = ctk.CTkLabel(app, text="", justify=tk.LEFT, wraplength=500)
-result_label.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+result_label = ctk.CTkTextbox(app)
+result_label.insert("0.0","")
+result_label.grid(row=10,column=1,sticky="nsew")
+result_label.configure(state="disabled",width = 350)
+
 
 app.mainloop()
